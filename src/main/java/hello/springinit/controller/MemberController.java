22f975608bcd08ff567862abe14b6eaec3ec5679
hil <hil679922@gmail.com>
2023-03-13 02:05:01 +0900

    Add: MemberController

---
+++ src/main/java/hello/springinit/controller/MemberController.java
@@ -0,0 +1,12 @@
+package hello.springinit.controller;
+import hello.springinit.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
